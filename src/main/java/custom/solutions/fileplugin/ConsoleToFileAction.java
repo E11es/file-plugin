3bad9a67ea3968533013a1980a4a2d8edd8efe86
toshqaaaa <Lebe9@mail.ru>
2024-09-22 22:11:39 +0300

    - try to save file through psi-api - added fileChooser menu (not working)

--- src/main/java/custom/solutions/fileplugin/ConsoleToFileAction.java
+++ src/main/java/custom/solutions/fileplugin/ConsoleToFileAction.java
@@ -4,10 +4,21 @@ import com.intellij.execution.ui.ConsoleView;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.fileChooser.FileChooserFactory;
+import com.intellij.openapi.fileChooser.FileSaverDescriptor;
+import com.intellij.openapi.fileChooser.FileSaverDialog;
+import com.intellij.openapi.fileTypes.FileTypes;
 import com.intellij.openapi.project.DumbAwareAction;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFileWrapper;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiFileFactory;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 /**
  * Action.
@@ -42,16 +53,41 @@ public class ConsoleToFileAction extends DumbAwareAction {
      */
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        /*
-        Steps:
-        1. Create JPanel
-        2. Configure file creation (name, path, etc) in JPanel
-        3. Create file with configuration provided.
-         */
+        FileSaverDescriptor descriptor = new FileSaverDescriptor("Save As", "Save console input");
 
 //        ConsoleView data = e.getData(LangDataKeys.CONSOLE_VIEW);
-//        Project project = e.getProject();
-//        DataContext dataContext = e.getDataContext();
+        Caret caret = e.getData(LangDataKeys.CARET);
+//        No data can be provided through data-methods
+//        boolean isDataPresented = data != null && data.getContentSize() > 0;
+        boolean isCaretSelectionPresent = caret != null && caret.getSelectedText() != null;
+
+
+        String initialContent = null;
+        //TODO: how to get all data from console view?
+//        if (isDataPresented) {
+//            initialContent = data.toString();
+//        } else
+
+        if (isCaretSelectionPresent) {
+            initialContent = caret.getSelectedText();
+        }
+
+        Project project = Objects.requireNonNull(e.getData(CommonDataKeys.PROJECT));
+        PsiFile file = PsiFileFactory.getInstance(project)
+                .createFileFromText(
+                        "my_fancy_name.txt.ft",
+                        FileTypes.PLAIN_TEXT,
+                        initialContent,
+                        0,
+                        true);
+
+        // FIXME: no save action performed. Maybe access? Or incorrect usage of saveFileDialog? Maybe there is another API?
+        FileSaverDialog saveFileDialog = FileChooserFactory.getInstance().createSaveFileDialog(descriptor, project);
+        VirtualFileWrapper result = saveFileDialog.save(file.getVirtualFile(), file.getName());
+
+/*        About creation of VFS: Usually, you don't (create VFS).
+          As a general rule, files are created either through the PSI API or through the regular java.io.File API
+ */
 
     }
 
