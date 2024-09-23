ddfe950e8b867173e867bb6c6330eb463d70f119
toshqaaaa <Lebe9@mail.ru>
2024-09-23 23:01:05 +0300

    - add todo and thoughts

--- src/main/java/custom/solutions/fileplugin/ConsoleToFileAction.java
+++ src/main/java/custom/solutions/fileplugin/ConsoleToFileAction.java
@@ -1,24 +1,14 @@
 package custom.solutions.fileplugin;
 
+import com.intellij.execution.impl.ConsoleViewImpl;
 import com.intellij.execution.ui.ConsoleView;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.editor.Caret;
-import com.intellij.openapi.fileChooser.FileChooserFactory;
-import com.intellij.openapi.fileChooser.FileSaverDescriptor;
-import com.intellij.openapi.fileChooser.FileSaverDialog;
-import com.intellij.openapi.fileTypes.FileTypes;
 import com.intellij.openapi.project.DumbAwareAction;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFileWrapper;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiFileFactory;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Objects;
 
 /**
  * Action.
@@ -46,6 +36,7 @@ public class ConsoleToFileAction extends DumbAwareAction {
         e.getPresentation().setEnabled(isDataPresented || isCaretSelectionPresent);
     }
 
+
     /**
      * An action logic itself.
      *
@@ -53,42 +44,21 @@ public class ConsoleToFileAction extends DumbAwareAction {
      */
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        FileSaverDescriptor descriptor = new FileSaverDescriptor("Save As", "Save console input");
-
-//        ConsoleView data = e.getData(LangDataKeys.CONSOLE_VIEW);
+        ConsoleView data = e.getData(LangDataKeys.CONSOLE_VIEW);
         Caret caret = e.getData(LangDataKeys.CARET);
-//        No data can be provided through data-methods
-//        boolean isDataPresented = data != null && data.getContentSize() > 0;
+        //TODO: differentiate caret data and all console input provided. Maybe two actions or smth like. Extract to
+        // method and from above.
+        boolean isDataPresented = data != null && data.getContentSize() > 0;
         boolean isCaretSelectionPresent = caret != null && caret.getSelectedText() != null;
+        //TODO: can we avoid cast at data? Is it a good idea?
+        String initialContent = isDataPresented ? ((ConsoleViewImpl) data).getText() : caret.getSelectedText();
 
+        /*
+        There are some problems: if we want create a file, but PSI-provides logic for version control. It's not a
+        good idea for our solution. Create Document or VirtualFile not good idea too (according to official
+         documentation). We can use standard java File-api (approved by documentation of plugin development).
 
-        String initialContent = null;
-        //TODO: how to get all data from console view?
-//        if (isDataPresented) {
-//            initialContent = data.toString();
-//        } else
-
-        if (isCaretSelectionPresent) {
-            initialContent = caret.getSelectedText();
-        }
-
-        Project project = Objects.requireNonNull(e.getData(CommonDataKeys.PROJECT));
-        PsiFile file = PsiFileFactory.getInstance(project)
-                .createFileFromText(
-                        "my_fancy_name.txt.ft",
-                        FileTypes.PLAIN_TEXT,
-                        initialContent,
-                        0,
-                        true);
-
-        // FIXME: no save action performed. Maybe access? Or incorrect usage of saveFileDialog? Maybe there is another API?
-        FileSaverDialog saveFileDialog = FileChooserFactory.getInstance().createSaveFileDialog(descriptor, project);
-        VirtualFileWrapper result = saveFileDialog.save(file.getVirtualFile(), file.getName());
-
-/*        About creation of VFS: Usually, you don't (create VFS).
-          As a general rule, files are created either through the PSI API or through the regular java.io.File API
- */
-
+         */
     }
 
     //TODO: obtain alternatives. What is the point?
